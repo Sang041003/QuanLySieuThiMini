c603c3788ef3c2a9650766e44cca278a40b3c1b4
Sang <nnsang041003@gmail.com>
2023-07-24 19:29:06 +0700

    Add project files.

---
+++ QuanLySieuThiMini/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuanLySieuThiMini.Models;
+
+namespace QuanLySieuThiMini.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDBContext  _dbContext;
+
+        public ProductController(ProductDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var products = _dbContext.Products.ToList();
+            return PartialView("Index", products);
+        }
+    }
+}
---
+++ QuanLySieuThiMini/Models/DataEntryClerk.cs
@@ -0,0 +1,29 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLySieuThiMini.Models{
+    public class DataEntryClerk : Employee {
+
+        public DataEntryClerk() {
+        }
+
+        public void addProduct(Product newPro) {
+
+        }
+
+        public void addGuest(Guest guest) {
+        }
+
+        public void updateInventory(int proID, int quantity) {
+
+        }
+
+        public void addEmployee(Employee emp) {
+
+        }
+
+    }
+}
